1bef665348d1b994d9b852871fb6f28909929ed7
phadel <fadloulfadla92@gmail.com>
2018-01-31 13:21:44 +0100

    Partition Astro RDD

---
+++ Projet-one/src/main/java/com/partition/file/partition/AstroRecord.java
@@ -0,0 +1,361 @@
+package com.partition.file.partition;
+
+
+
+public class AstroRecord{
+
+	 private int  nc;
+	 private int nt;
+	 private int host;
+	 private double alpha;
+	 private double delta;
+	 private double distance;
+	 private double muAlpha;
+	 private double muDelta8;
+	 private double radialVelocity;
+	 private double magG;
+	 private double magGBp;
+	 private double magGRp;
+	 private double magGRvs;
+	 private double Av;
+	 private double Ag;
+	 private double Rv;
+	 private double meanAbsoluteV9;
+	 private double colorVminusI;
+	 private double orbitPeriod;
+	 private double periastronDate;
+	 private double semimajorAxis;
+	 private double eccentricity;
+	 private double periastronArgument;
+	 private double inclination;
+	 private double longitudeAscendingNode;
+	 private double phase;
+	 private int flagintegereracting;
+	 private int population;
+	 private double age;
+	 private double feH;
+	 private double alphaFe;
+	 private double mbol;
+	 private double mass;
+	 private double radius;
+	 private double teff;
+	 private double logg;
+	 private String spectralType;
+	 private double vsini;
+	 private double rEnvRStar;
+	 private double bondAlbedo;
+	 private double geomAlbedo;
+	 private String variabilityType;
+	 private double variabilityAmplitude;
+	 private double variabilityPeriod;
+	 private double variabilityPhase;
+	 private boolean hasPhotocenterMotion;
+	 private String openClusterName;
+	 private String sourceExtendedId;
+	 private String sourceId;
+	 private String healpix;
+	 
+	 
+	public int getNc() {
+		return nc;
+	}
+	public void setNc(int nc) {
+		this.nc = nc;
+	}
+	public int getNt() {
+		return nt;
+	}
+	public void setNt(int nt) {
+		this.nt = nt;
+	}
+	public int getHost() {
+		return host;
+	}
+	public void setHost(int host) {
+		this.host = host;
+	}
+	public double getAlpha() {
+		return alpha;
+	}
+	public void setAlpha(double alpha) {
+		this.alpha = alpha;
+	}
+	public double getDelta() {
+		return delta;
+	}
+	public void setDelta(double delta) {
+		this.delta = delta;
+	}
+	public double getDistance() {
+		return distance;
+	}
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+	public double getMuAlpha() {
+		return muAlpha;
+	}
+	public void setMuAlpha(double muAlpha) {
+		this.muAlpha = muAlpha;
+	}
+	public double getMuDelta8() {
+		return muDelta8;
+	}
+	public void setMuDelta8(double muDelta8) {
+		this.muDelta8 = muDelta8;
+	}
+	public double getRadialVelocity() {
+		return radialVelocity;
+	}
+	public void setRadialVelocity(double radialVelocity) {
+		this.radialVelocity = radialVelocity;
+	}
+	public double getMagG() {
+		return magG;
+	}
+	public void setMagG(double magG) {
+		this.magG = magG;
+	}
+	public double getMagGBp() {
+		return magGBp;
+	}
+	public void setMagGBp(double magGBp) {
+		this.magGBp = magGBp;
+	}
+	public double getMagGRp() {
+		return magGRp;
+	}
+	public void setMagGRp(double magGRp) {
+		this.magGRp = magGRp;
+	}
+	public double getMagGRvs() {
+		return magGRvs;
+	}
+	public void setMagGRvs(double magGRvs) {
+		this.magGRvs = magGRvs;
+	}
+	public double getAv() {
+		return Av;
+	}
+	public void setAv(double av) {
+		Av = av;
+	}
+	public double getAg() {
+		return Ag;
+	}
+	public void setAg(double ag) {
+		Ag = ag;
+	}
+	public double getRv() {
+		return Rv;
+	}
+	public void setRv(double rv) {
+		Rv = rv;
+	}
+	public double getMeanAbsoluteV9() {
+		return meanAbsoluteV9;
+	}
+	public void setMeanAbsoluteV9(double meanAbsoluteV9) {
+		this.meanAbsoluteV9 = meanAbsoluteV9;
+	}
+	public double getColorVminusI() {
+		return colorVminusI;
+	}
+	public void setColorVminusI(double colorVminusI) {
+		this.colorVminusI = colorVminusI;
+	}
+	public double getOrbitPeriod() {
+		return orbitPeriod;
+	}
+	public void setOrbitPeriod(double orbitPeriod) {
+		this.orbitPeriod = orbitPeriod;
+	}
+	public double getPeriastronDate() {
+		return periastronDate;
+	}
+	public void setPeriastronDate(double periastronDate) {
+		this.periastronDate = periastronDate;
+	}
+	public double getSemimajorAxis() {
+		return semimajorAxis;
+	}
+	public void setSemimajorAxis(double semimajorAxis) {
+		this.semimajorAxis = semimajorAxis;
+	}
+	public double getEccentricity() {
+		return eccentricity;
+	}
+	public void setEccentricity(double eccentricity) {
+		this.eccentricity = eccentricity;
+	}
+	public double getPeriastronArgument() {
+		return periastronArgument;
+	}
+	public void setPeriastronArgument(double periastronArgument) {
+		this.periastronArgument = periastronArgument;
+	}
+	public double getInclination() {
+		return inclination;
+	}
+	public void setInclination(double inclination) {
+		this.inclination = inclination;
+	}
+	public double getLongitudeAscendingNode() {
+		return longitudeAscendingNode;
+	}
+	public void setLongitudeAscendingNode(double longitudeAscendingNode) {
+		this.longitudeAscendingNode = longitudeAscendingNode;
+	}
+	public double getPhase() {
+		return phase;
+	}
+	public void setPhase(double phase) {
+		this.phase = phase;
+	}
+	public int getFlagintegereracting() {
+		return flagintegereracting;
+	}
+	public void setFlagintegereracting(int flagintegereracting) {
+		this.flagintegereracting = flagintegereracting;
+	}
+	public int getPopulation() {
+		return population;
+	}
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+	public double getAge() {
+		return age;
+	}
+	public void setAge(double age) {
+		this.age = age;
+	}
+	public double getFeH() {
+		return feH;
+	}
+	public void setFeH(double feH) {
+		this.feH = feH;
+	}
+	public double getAlphaFe() {
+		return alphaFe;
+	}
+	public void setAlphaFe(double alphaFe) {
+		this.alphaFe = alphaFe;
+	}
+	public double getMbol() {
+		return mbol;
+	}
+	public void setMbol(double mbol) {
+		this.mbol = mbol;
+	}
+	public double getMass() {
+		return mass;
+	}
+	public void setMass(double mass) {
+		this.mass = mass;
+	}
+	public double getRadius() {
+		return radius;
+	}
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	public double getTeff() {
+		return teff;
+	}
+	public void setTeff(double teff) {
+		this.teff = teff;
+	}
+	public double getLogg() {
+		return logg;
+	}
+	public void setLogg(double logg) {
+		this.logg = logg;
+	}
+	public String getSpectralType() {
+		return spectralType;
+	}
+	public void setSpectralType(String spectralType) {
+		this.spectralType = spectralType;
+	}
+	public double getVsini() {
+		return vsini;
+	}
+	public void setVsini(double vsini) {
+		this.vsini = vsini;
+	}
+	public double getrEnvRStar() {
+		return rEnvRStar;
+	}
+	public void setrEnvRStar(double rEnvRStar) {
+		this.rEnvRStar = rEnvRStar;
+	}
+	public double getBondAlbedo() {
+		return bondAlbedo;
+	}
+	public void setBondAlbedo(double bondAlbedo) {
+		this.bondAlbedo = bondAlbedo;
+	}
+	public double getGeomAlbedo() {
+		return geomAlbedo;
+	}
+	public void setGeomAlbedo(double geomAlbedo) {
+		this.geomAlbedo = geomAlbedo;
+	}
+	public String getVariabilityType() {
+		return variabilityType;
+	}
+	public void setVariabilityType(String variabilityType) {
+		this.variabilityType = variabilityType;
+	}
+	public double getVariabilityAmplitude() {
+		return variabilityAmplitude;
+	}
+	public void setVariabilityAmplitude(double variabilityAmplitude) {
+		this.variabilityAmplitude = variabilityAmplitude;
+	}
+	public double getVariabilityPeriod() {
+		return variabilityPeriod;
+	}
+	public void setVariabilityPeriod(double variabilityPeriod) {
+		this.variabilityPeriod = variabilityPeriod;
+	}
+	public double getVariabilityPhase() {
+		return variabilityPhase;
+	}
+	public void setVariabilityPhase(double variabilityPhase) {
+		this.variabilityPhase = variabilityPhase;
+	}
+	public boolean isHasPhotocenterMotion() {
+		return hasPhotocenterMotion;
+	}
+	public void setHasPhotocenterMotion(boolean hasPhotocenterMotion) {
+		this.hasPhotocenterMotion = hasPhotocenterMotion;
+	}
+	public String getOpenClusterName() {
+		return openClusterName;
+	}
+	public void setOpenClusterName(String openClusterName) {
+		this.openClusterName = openClusterName;
+	}
+	public String getSourceExtendedId() {
+		return sourceExtendedId;
+	}
+	public void setSourceExtendedId(String sourceExtendedId) {
+		this.sourceExtendedId = sourceExtendedId;
+	}
+	public String getSourceId() {
+		return sourceId;
+	}
+	public void setSourceId(String sourceId) {
+		this.sourceId = sourceId;
+	}
+	public String getHealpix() {
+		return healpix;
+	}
+	public void setHealpix(String healpix) {
+		this.healpix = healpix;
+	}
+	 
+	 
+}
---
+++ Projet-one/src/main/java/com/partition/file/partition/PartitionRdd.java
@@ -0,0 +1,197 @@
+package com.partition.file.partition;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Column;
+//import org.apache.spark.sql.Column;
+//import org.apache.spark.network.protocol.Encoders;
+import org.apache.spark.sql.Dataset;
+//import org.apache.spark.sql.Encoder;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.api.java.UDF1;
+
+import healpix.essentials.*;
+//import org.apache.spark.sql.expressions.UserDefinedAggregateFunction;
+//import org.apache.spark.sql.expressions.UserDefinedFunction;
+import org.apache.spark.sql.types.DataTypes;
+//import org.apache.spark.sql.types.LongType;
+//import scala.collection.Seq;
+import healpix.essentials.Scheme;
+
+
+
+
+public class PartitionRdd {
+	
+	
+	//private static final String LongType = null;
+
+	public static void main(String[] args) {
+	    SparkSession spark = SparkSession
+	      .builder()
+	      .appName("Algo de Partitionnement RDD")
+	      .config("spark.master", "local")
+	      .getOrCreate();
+
+	    
+	    AstroRecordCsv(spark);
+	    spark.stop();
+	    
+	    
+	  }
+	
+	 private static void AstroRecordCsv(SparkSession spark) {
+		 
+	// Création d'un RDD AstoRecord depuis notre csv
+	JavaRDD<AstroRecord> astroRecordRDD = spark.read()
+	  .textFile("/home/vaadl/Bureau/test/N050k.csv")
+	  .javaRDD()
+	  .map(line -> {
+	    String[] parts = line.split(",");
+	    AstroRecord astrorecord = new AstroRecord();
+	    astrorecord.setNc(Integer.parseInt (parts[0].trim()));
+	    astrorecord.setNt(Integer.parseInt(parts[1].trim()));
+	    astrorecord.setHost(Integer.parseInt(parts[2].trim()));
+	    astrorecord.setAlpha(Double.parseDouble(parts[3].trim()));
+	    astrorecord.setDelta(Double.parseDouble(parts[4].trim()));
+	    astrorecord.setDistance(Double.parseDouble(parts[5].trim()));
+	    astrorecord.setMuAlpha(Double.parseDouble(parts[6].trim()));
+	    astrorecord.setMuDelta8(Double.parseDouble(parts[7].trim()));
+	    astrorecord.setRadialVelocity(Double.parseDouble(parts[8].trim()));
+	    astrorecord.setMagG(Double.parseDouble(parts[9].trim()));
+	    astrorecord.setMagGBp(Double.parseDouble(parts[10].trim()));
+	    astrorecord.setMagGRp(Double.parseDouble(parts[11].trim()));
+	    astrorecord.setMagGRvs(Double.parseDouble(parts[12].trim()));
+	    astrorecord.setAv(Double.parseDouble(parts[13].trim()));
+	    astrorecord.setAg(Double.parseDouble(parts[14].trim()));
+	    astrorecord.setRv(Double.parseDouble(parts[15].trim()));
+	    astrorecord.setMeanAbsoluteV9(Double.parseDouble(parts[16].trim()));
+	    astrorecord.setColorVminusI(Double.parseDouble(parts[17].trim()));
+	    astrorecord.setOrbitPeriod(Double.parseDouble(parts[18].trim()));
+	    astrorecord.setPeriastronDate(Double.parseDouble(parts[19].trim()));
+	    astrorecord.setSemimajorAxis(Double.parseDouble(parts[20].trim()));
+	    astrorecord.setEccentricity(Double.parseDouble(parts[21].trim()));
+	    astrorecord.setPeriastronArgument(Double.parseDouble(parts[22].trim()));
+	    astrorecord.setInclination(Double.parseDouble(parts[23].trim()));
+	    astrorecord.setLongitudeAscendingNode(Double.parseDouble(parts[24].trim()));
+	    astrorecord.setPhase(Double.parseDouble(parts[25].trim()));
+	    astrorecord.setFlagintegereracting(Integer.parseInt (parts[26].trim()));
+	    astrorecord.setPopulation(Integer.parseInt (parts[27].trim()));
+	    astrorecord.setAge(Double.parseDouble(parts[28].trim()));
+	    astrorecord.setFeH(Double.parseDouble(parts[29].trim()));
+	    astrorecord.setAlphaFe(Double.parseDouble(parts[30].trim()));
+	    astrorecord.setMbol(Double.parseDouble(parts[31].trim()));
+	    astrorecord.setMass(Double.parseDouble(parts[32].trim()));
+	    astrorecord.setRadius(Double.parseDouble(parts[33].trim()));
+	    astrorecord.setTeff(Double.parseDouble(parts[34].trim()));
+	    astrorecord.setLogg(Double.parseDouble(parts[35].trim()));
+	    astrorecord.setSpectralType(parts[36]);
+	    astrorecord.setVsini(Double.parseDouble(parts[37].trim()));
+	    astrorecord.setrEnvRStar(Double.parseDouble(parts[38].trim()));
+	    astrorecord.setBondAlbedo(Double.parseDouble(parts[39].trim()));
+	    astrorecord.setGeomAlbedo(Double.parseDouble(parts[40].trim()));
+	    astrorecord.setVariabilityType(parts[41]);
+	    astrorecord.setVariabilityAmplitude(Double.parseDouble(parts[42].trim()));
+	    astrorecord.setVariabilityPeriod(Double.parseDouble(parts[43].trim()));
+	    astrorecord.setVariabilityPhase(Double.parseDouble(parts[44].trim()));
+	    astrorecord.setHasPhotocenterMotion(Boolean.parseBoolean(parts[45].trim()));
+	    astrorecord.setOpenClusterName(parts[46]);
+	    astrorecord.setSourceExtendedId(parts[47]);
+	    astrorecord.setSourceId(parts[48]);
+	    astrorecord.setHealpix(parts[49]);
+	    return astrorecord;
+	  });
+	
+	
+	//Application du schéma au RDD de JavaBeans pour obtenir un DataFrame
+	Dataset<Row> astroDF = spark.createDataFrame(astroRecordRDD, AstroRecord.class);
+	
+	astroDF
+	.write()
+	.format("parquet")
+ 	.save("/home/vaadl/Bureau/test/astroDF.parquet");
+	//astroDF.show();
+	Dataset<Row>  astroDFparquet=spark.sql("SELECT * FROM parquet.`/home/vaadl/Bureau/test/astroDF.parquet`");
+	//astroDFparquet.show();
+	
+	//Espace insuffisant
+	/*astroDF
+	.write()
+	.partitionBy("alpha")
+	.saveAsTable("partitions_alpha_N050k");*/
+	
+	//astroDF.write().parquet("/home/vaadl/Bureau/test/astroRDD.parquet");
+	
+	// Push du DF dans une vue
+	astroDFparquet.createOrReplaceTempView("astro");
+	
+	// Essais SQL
+	Dataset<Row> sqlDF = spark.sql("SELECT alpha, delta FROM astro");
+	System.out.printf("\n");
+	System.out.printf("********Affichage des coordonnées de l'étoile alpha & delta********");
+	System.out.printf("\n");
+	sqlDF.show();
+	
+	// Utilisation des UDFs
+	spark.udf().register("COORDO", new UDF1<Long, Long>() {
+		  /**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		  public Long call(Long alpha) {
+		    return (((long) alpha));
+		  }
+		}, DataTypes.LongType);
+	spark.sql("SELECT alpha, COORDO(alpha) AS alphaPix FROM astro").show();
+    	
+    	//Dataset<Row> coordoDF = astroDFparquet.withColumn("alphaTmp", astroDF.col("alpha").cast(LongType)).drop("alpha").withColumnRenamed("alphaTmp", "alpha");   
+    	//coordoDF = astroDFparquet.withColumn("deltaTmp", astroDF.col("delta").cast(LongType)).drop("delta").withColumnRenamed("deltaTmp", "delta");   
+    	
+    	//coordoDF.show();
+    	
+    	//coordoDF
+    	//.write()
+    	//.format("parquet")
+     	//.save("/home/vaadl/Bureau/test/coordoDF.parquet");
+    	
+    	
+    	
+    	
+    	
+    	
+    	/*astroDFparquet.col("alpha.field");
+    	astroDFparquet.col("delta.field");
+    	astroDFparquet.show();*/
+    	
+    	//long alphaPx;
+    	
+    	//alphaPix=alphaPx;
+    	//alphaPx=alphaPix;
+		//Column deltaPix = coordoDF.col("deltaTmp");
+    	
+	//	long  alphaPixx = alphaPix.Field<long>("alphaTmp"); 
+    	
+	//Partitionnement healpix
+	System.out.printf("Usage healpix");
+    int nSide = 16;
+    
+    try {
+    	HealpixBase healpixBase = new HealpixBase(nSide, Scheme.NESTED);
+    	
+    	
+    	//double theta = Math.PI / 2 - Math.toRadians(delta);
+    	//double phi = Math.toRadians(alpha);
+    	
+    	//long res = healpixBase.ang2pix(new Pointing(alphaPix, deltaPix));
+        // System.out.printf("Pix %d= %d", nSide, res);
+    	
+    }
+    catch(Exception e){
+    	e.printStackTrace();
+    }
+	
+	 }
+
+}
